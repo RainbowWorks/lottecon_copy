16d23ad0a3c1df39492a22f9b65fa6be1a6a4b2b
RainbowWorks <kuj4235@kakao.com>
2025-04-21 16:00:48 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.main_visual_slide').slick({
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
+        autoplaySpeed: 6000,
+        speed: 1000,
+    });
+
+});
