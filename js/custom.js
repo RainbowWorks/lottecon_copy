8dce284acbac43ec3c3d059b177419df640b7de3
RainbowWorks <kuj4235@kakao.com>
2025-04-22 15:57:48 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,65 +1,34 @@
 $(function () {
-    $('.main_visual_slide').slick({
-        arrows: false,
-        autoplay: true,
-        pauseOnHover: false,
-        autoplaySpeed: 6000,
-        speed: 1000,
-    });
+    const MMS = new Swiper('.main_menu_slide', {/*옵션입력*/
+        loop: true,
+        slidesPerView: 4, /*swiperjs에서 받아왔음*/
+        spaceBetween: 80, /*swiperjs에서 받아왔음*/
+        autoplay: {
+            delay: 0, /*0이면 머무르는 시간이 0초*/
+            disableOnInteraction: false,
+        }, /*swiperjs에서 받아왔음*/
 
-
-    $('.main_visual .arrows .prev').on('click', function () {
-        $('.main_visual_slide').slick('slickPrev');
-    });
-
-    $('.main_visual .arrows .next').on('click', function () {
-        $('.main_visual_slide').slick('slickNext');
-    });
-
-
-    $('.main_product_slide').slick({
-        slidesToShow: 4,
-        arrows: false,
-        dots: true,
-    });
-
-
-    $('.main_product .arrows .prev').on('click', function () {
-        $('.main_product_slide').slick('slickPrev');
-    });
-
-    $('.main_product .arrows .next').on('click', function () {
-        $('.main_product_slide').slick('slickNext');
-    });
-
-});
-
-
-$(function () {
-    $('.main_content .tab_menu button').on('click', function () {
-        let idx = $(this).index(); // 클릭한 것의 번호를 가져와서 저장함
-        console.log(idx);
-
-        $('.main_content .tab_menu button').removeClass('on');
-        $(this).addClass('on');
-
-        $('.main_content .tab_content .con').removeClass('on');
-        $('.main_content .tab_content .con').eq(idx).addClass('on');
+        speed: 5000, /*2500은 2.5초이다*/
     });
 });
 
-
 $(function () {
-    $('.to_top').on('click', function () {
-        $('html, body').animate({ scrollTop: 0 }, 1000)
-    })
-});
 
-$(function () {
-    $('.footer #lnk').on('change', function () {
-        let lnk = $(this).val();
-        if (lnk) {
-            window.open(lnk)
+    //스크롤이 됐을 떄 header에 on을 붙인다.
+    //스크롤이 0이면 header에서 on을 땐다.
+    //event : click, scroll, wheel, mouseenter, mouseleave, change
+
+    $(window).on('scroll', function () {
+        //스크롤 된 양을 구함.
+        let sct = $(window).scrollTop();
+        console.log(sct)
+
+        if (sct > 0) {
+            $('#header').addClass('on')
+        } else {
+            $('#header').removeClass('on')
         }
-    })
-})
+    });
+
+
+});
