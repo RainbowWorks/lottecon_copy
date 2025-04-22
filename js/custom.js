9bc5f5bb5fd78e007de9188feed351c05c6f0fb6
RainbowWorks <kuj4235@kakao.com>
2025-04-22 16:00:10 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,20 @@
 $(function () {
-    const MMS = new Swiper('.main_menu_slide', {/*옵션입력*/
+    $('.main_visual_slide').slick({
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
+        autoplaySpeed: 6000,
+        speed: 1000,
+    });
+
+
+})
+
+$(function () {
+    const MMS = new Swiper('.main_pr_slide', {/*옵션입력*/
         loop: true,
-        slidesPerView: 4, /*swiperjs에서 받아왔음*/
-        spaceBetween: 80, /*swiperjs에서 받아왔음*/
+        slidesPerView: 6, /*swiperjs에서 받아왔음*/
+        spaceBetween: 30, /*swiperjs에서 받아왔음*/
         autoplay: {
             delay: 0, /*0이면 머무르는 시간이 0초*/
             disableOnInteraction: false,
@@ -10,25 +22,4 @@ $(function () {
 
         speed: 5000, /*2500은 2.5초이다*/
     });
-});
-
-$(function () {
-
-    //스크롤이 됐을 떄 header에 on을 붙인다.
-    //스크롤이 0이면 header에서 on을 땐다.
-    //event : click, scroll, wheel, mouseenter, mouseleave, change
-
-    $(window).on('scroll', function () {
-        //스크롤 된 양을 구함.
-        let sct = $(window).scrollTop();
-        console.log(sct)
-
-        if (sct > 0) {
-            $('#header').addClass('on')
-        } else {
-            $('#header').removeClass('on')
-        }
-    });
-
-
 });
